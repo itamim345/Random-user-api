bc4fafe7c13ddbd15f31de843539e4304e6bd413
AF Tamim <itamim345@gmail.com>
2022-10-06 23:30:39 +0530

    Modified JS

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ function displayFriend (friend){
     <img src="${friend.results[0].picture.large}" class="card-img-top img-thumbnail" style="height: 12rem">
     <div class="card-body">
       <h5 class="card-title">${friend.results[0].name.first +" " + friend.results[0].name.last} </h5>
-      <h6><strong>Email</strong> : ${friend.results[0].email} </h6>
       <h6><strong>Gender</strong> : ${friend.results[0].gender} </h6>
       <h6><strong>Age</strong> : ${friend.results[0].dob.age} </h6>
       <h6><strong>City</strong> : ${friend.results[0].location.city} </h6>
       <h6><strong>Country</strong> : ${friend.results[0].location.country} </h6>
+      <em class="text-danger">Press Button agian to meet different friend</em>
     </div>
   </div>`
 }
